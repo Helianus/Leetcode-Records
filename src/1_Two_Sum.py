67932e1f6efbd7c9a90bd9f7554103c5b404a11c
Helianus <xinyuhelianushu@gmail.com>
2019-05-20 22:22:39 -0500

    Update 1_Two_Sum.py

--- src/1_Two_Sum.py
+++ src/1_Two_Sum.py
@@ -12,8 +12,7 @@
 def twoSum_bruteForce(nums, target):
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
-            sum = nums[i] + nums[j]
-            if sum == target:
+            if (nums[i] + nums[j]) == target:
                 return [i, j]
 
 print(twoSum_bruteForce([2, 7, 11, 15], 9))
